c37aeec1872ca85bf5eb63a0de94eede2686aa23
maclovr <maclovr75@gmail.com>
2014-11-30 00:18:00 +0100

    [wip]

--- src/Framework/Provider/ConfigServiceProvider.php
+++ src/Framework/Provider/ConfigServiceProvider.php
@@ -14,31 +14,22 @@ use Symfony\Component\Routing\Loader\YamlFileLoader;
 class ConfigServiceProvider implements ServiceProviderInterface
 {
     const APP_DIRECTORY    = 'app';
-    const CACHE_DIRECTORY  = 'cache';
     const CONFIG_DIRECTORY = 'config';
-
-    // to switch between prod & dev
-    // just set the APP_ENV environment variable:
-    // in apache: SetEnv APP_ENV dev
-    // in nginx with fcgi: fastcgi_param APP_ENV dev
-    const CONFIG_ROUTES_FILE = 'routing.yml';
 
     /**
      * {@inheritdoc}
      */
     public function register(Container $app)
     {
-        $app['root_dir'] = realpath(__DIR__ . '/../../../../');
-        $configDirectories = [ $app['root_dir'] . '/' . self::APP_DIRECTORY . '/' . self::CONFIG_DIRECTORY];
-
-//        print_r($configDirectories);
-//        exit;
+        echo dirname(dirname(__FILE__));
+        exit;
+        $path =  realpath(dirname(__DIR__) . '/../../../') . '/' . self::APP_DIRECTORY . '/' . self::CONFIG_DIRECTORY;
+        $configDirectories = [$path];
         $locator = new FileLocator($configDirectories);
 
-        // load only *.yml files?
-        $app['config_loader'] = function () use ($locator) {
+        $app['config_loader'] = function () use ($app, $locator) {
+            // load only *.yml files?
             $loader =  new YamlFileLoader($locator);
-
             return $loader;
         };
     }
