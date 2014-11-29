e29ee792d08826d8bbab9fa4c934064ba2624429
thorenx <Thorenx@hotmail.fr>
2014-11-29 16:48:44 +0100

    avancement du modèle de données

---
+++ app/Model/Core/Menu.php
@@ -0,0 +1,17 @@
+<?php
+namespace Nkstamina\Model\Core;
+
+/**
+ * Class Menu
+ *
+ * @package Nkstamina\Model\Core
+ */
+class Menu
+{
+
+    private $themeId;
+
+    private $menuId;
+
+    private $name;
+}
---
+++ app/Model/Core/UserGroupAssignation.php
@@ -0,0 +1,15 @@
+<?php
+namespace Nkstamina\Model\Core;
+
+/**
+ * Class UserGroup
+ *
+ * @package Nkstamina\Model\Core
+ */
+class UserGroupAssignation
+{
+
+    private $userGroupId;
+
+    private $userId;
+}
