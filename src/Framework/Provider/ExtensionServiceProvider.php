72a1158c54b390a6786068f736e57291638e0b1f
maclovr <maclovr75@gmail.com>
2014-12-15 22:05:48 +0100

    Deleted unused functions

--- src/Framework/Provider/ExtensionServiceProvider.php
+++ src/Framework/Provider/ExtensionServiceProvider.php
@@ -60,22 +60,9 @@ class ExtensionServiceProvider implements ServiceProviderInterface
             $extensionName                                  = $directory->getRelativePathname();
             $this->extensions[$extensionName]['name']       = $extensionName;
             $this->extensions[$extensionName]['pathName']   = $directory->getPathName();
-            $this->extensions[$extensionName]['parameters'] = $this->findExtensionParameters($app, $directory);
         }
 
         return $this->extensions;
-    }
-
-    /**
-     * Returns all parameters related to an extension
-     *
-     * @param $extensionName
-     *
-     * @return mixed
-     */
-    public function getExtensionParameters($extensionName)
-    {
-        return $this->extensions[$extensionName]['parameters'];
     }
 
     /**
@@ -98,43 +85,5 @@ class ExtensionServiceProvider implements ServiceProviderInterface
 
         return $directories;
     }
+}
 
-    /**
-     * Return config parameters for the current extension
-     *
-     * @param Container $app
-     * @param           $directory
-     *
-     * @throws Exception\InvalidConfigurationException
-     *
-     * @return array
-     */
-    private function findExtensionParameters(Container $app, $directory)
-    {
-        $parameters = [];
-        $yaml = $app['config.parser'];
-
-        $files = $app['config.finder']
-            ->files()
-            ->name('*.yml')
-            ->in($directory->getPathName())
-        ;
-
-        foreach ($files as $file) {
-
-            try {
-                $parameters[$file->getRelativePathname()] = [$yaml->parse(file_get_contents($file->getPathName()))];
-
-            } catch(ParseException $e) {
-                printf("Unable to parse the YAML string: %s", $e->getMessage());
-            }
-
-            // @wip do we have to translate this?
-            /*if (!isset($parameters[$file->getRelativePathname()][0]['parameters'])) {
-                throw new InvalidConfigurationException(sprintf('The key "parameters" must be defined in your file %s', $file->getPathName()));
-            }*/
-        }
-
-        return $parameters;
-    }
-} 
