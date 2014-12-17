e1e02635606fab4a2070ddf0338fab6f3d740565
thorenx <Thorenx@hotmail.fr>
2014-12-17 21:42:31 +0100

    mise à jour des news

--- app/Model/Extensions/News/News.php
+++ app/Model/Extensions/News/News.php
@@ -4,7 +4,7 @@ namespace Nkstamina\Model\Extensions\News;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity 
+ * @ORM\Entity
  */
 class News
 {
@@ -23,7 +23,7 @@ class News
 
     /**
      * @ORM\OneToOne(targetEntity="Nkstamina\Model\Core\User", cascade={"persist"})
-     */    
+     */
     protected $author;
 
     /**
