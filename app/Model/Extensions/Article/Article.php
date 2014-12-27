0a2a93aaa56da67cd51bc9718aecd59dbc4dfab9
thorenx <Thorenx@hotmail.fr>
2014-12-27 13:55:01 +0100

    nettoyage du code pour PR

---
+++ app/Model/Extensions/Article/Article.php
@@ -0,0 +1,43 @@
+<?php
+namespace Nkstamina\Model\Extensions\Article;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Article
+{
+     
+         /**
+          * @ORM\Column(name="id", type="integer")
+          * @ORM\Id
+          * @ORM\GeneratedValue(strategy="AUTO")
+          */
+         private $id;
+     
+         /**
+          * @ORM\OneToOne(targetEntity="Nkstamina\Model\Extensions\Category")
+          */
+         private $category;
+     
+         /**
+          * @ORM\OneToMany(targetEntity="Nkstamina\Model\Core\User", mappedBy="id")
+          */
+         private $user;
+     
+         /**
+          * @ORM\Column(name="title", type="string", length=255)
+          */
+         private $title;
+     
+         /**
+          * @ORM\Column(name="text", type="string", length=1000)
+          */
+         private $text;
+     
+         /**
+          * @ORM\Column(name="date", type="datetime")
+          */
+         private $date;
+}
