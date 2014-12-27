786b52aedaf7af30256cd96792965fc6f3fee01f
thorenx <Thorenx@hotmail.fr>
2014-12-27 13:40:47 +0100

    ajout de bootstrap sur le thème et d'un menu de base

--- app/Model/Core/MenuItem.php
+++ app/Model/Core/MenuItem.php
@@ -10,8 +10,6 @@ use Doctrine\ORM\Mapping as ORM;
 class MenuItem
 {
 
-    protected $menuitemId;
-
     /**
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
@@ -20,12 +18,46 @@ class MenuItem
     protected $id;
 
     /**
-     * @ORM\OneToOne(targetEntity="Nkstamina\Model\Core\Block", cascade={"persist"})
+     * @ORM\OneToOne(targetEntity="Nkstamina\Model\Core\Menu", cascade={"persist"})
      */
-    protected $block;
+    protected $menu;
+    
+    /**
+     * @ORM\Column(name="link", type="string", length=255)
+     */
+    protected $link;
+    
+    /**
+     * @ORM\Column(name="text", type="string", length=255)
+     */    
+    protected $text;
 
     /**
      * @ORM\Column(name="order", type="integer")
      */
     protected $order;
+    
+    public function setId($id) {
+        $this->id = $id;
+    }
+    
+    public function getId() {
+        return $this->id;
+    }
+    
+    public function setLink($link) {
+        $this->link = $link;
+    }
+    
+    public function getLink() {
+        return $this->link;
+    }
+    
+    public function setText($text) {
+        $this->text = $text
+    }
+    
+    public function getText() {
+        return $this->text;
+    }
 }
